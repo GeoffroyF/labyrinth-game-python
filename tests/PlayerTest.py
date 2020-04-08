6e92a5d6e1ceb7d0132680018e724b712d9a0523
GeoffroyF <geoffroy.defelcourt@viacesi.fr>
2020-04-08 13:29:56 +0200

    tests

--- tests/PlayerTest.py
+++ tests/PlayerTest.py
@@ -1,6 +1,7 @@
 import unittest
 
 from impl.Player import Player
+from impl.game_saving.GameLoader import GameLoader
 from impl.labyrinth.Cell import Cell
 from impl.labyrinth.Labyrinth import Labyrinth
 from impl.objects.Treasure import Treasure
@@ -32,11 +33,6 @@ class PlayerTest(unittest.TestCase):
         x, y = player.get_pos()
         self.assertEqual(y == 1, True)
         self.assertEqual(x == 1, True)
-
-    def test_move_up(self):
-        labyrinth = Labyrinth(4)
-        player_pos_x, player_pos_y = labyrinth.generate_labyrinth()
-        player = Player(int(player_pos_x), int(player_pos_y))
 
 if __name__ == '__main__':
     unittest.main()
